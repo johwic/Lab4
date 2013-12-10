7f7dc5dc84f8ae441e8091e8a08bfbdc7dda4bcf
mateuszherczka <mateuszherczka@kth.fake>
2013-12-10 22:19:07 +0100

    Added matlab import and animation

--- Matlab/ReadParticles.m
+++ Matlab/ReadParticles.m
@@ -1,1 +1,15 @@
 P = load('particle_data.txt');
+stepCount = size(P,1);
+
+%% animate
+
+for i = 1:stepCount
+    plot( P(i,2:2:end), P(i,3:2:end),'*' );
+    axis([0 500 0 500]);
+    axis ij;
+    %drawnow; 
+    pause(.1);
+end
+
+%% stats
+
